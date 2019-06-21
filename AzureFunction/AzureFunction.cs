32cfedf105192258f700fa01cf1b37e6aabe3f8b
Dmitri Mogilevski <dmogilevski@gmail.com>
2019-06-21 17:37:04 -0500

    Added payload proj to contain the functional code for function to execute

--- AzureFunction/AzureFunction.cs
+++ AzureFunction/AzureFunction.cs
@@ -2,16 +2,17 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using Payload;
 
 namespace AzureFunction
 {
 	public static class AzureFunction
 	{
 		[FunctionName("AzureFunction")]
-		public static async Task Run([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer, ILogger log)
+		public static async Task RunAsync([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer, ILogger log)
 		{
-			log.LogInformation($"Azure Function triggered by timer. Executed at: {DateTime.Now}");
-			await Task.CompletedTask;
+			var payload = new FunctionalPayload(log);
+			await payload.ExecuteAsync();
 		}
 	}
 }
---
+++ Payload/FunctionalPayload.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Payload
+{
+	public class FunctionalPayload
+	{
+		private readonly ILogger _log;
+
+		public FunctionalPayload(ILogger log)
+		{
+			_log = log;
+		}
+
+		public async Task ExecuteAsync()
+		{
+			_log.LogInformation($"Azure Function triggered by timer. Executed at: {DateTime.Now}");
+
+			await Task.CompletedTask;
+		}
+	}
+}
